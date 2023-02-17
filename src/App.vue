c6201bca2c0f60ec8b67e137e2c9fbedacde7c7b
V S <vatosarmat@yandex.ru>
2023-02-17 17:11:38 +1000

    initiala commit, button

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+import Button from './components/Button.vue'
+</script>
+
+<template>
+  <div class="foo">
+    <Button>Create</Button>
+    <Button isLoading>Create</Button>
+  </div>
+  <HelloWorld msg="Vite + Vue" />
+</template>
+
+<style scoped>
+.foo {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  margin: auto;
+}
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ src/components/Button.vue
@@ -0,0 +1,70 @@
+<script setup lang="ts">
+const { isLoading } = defineProps<{ isLoading?: boolean }>()
+</script>
+
+<template>
+  <button :disabled="isLoading" :class="{ loading: isLoading }">
+    <span v-if="isLoading" class="loader"></span>
+    <slot v-else>Button</slot>
+  </button>
+</template>
+
+<style scoped lang="scss">
+$height: 48px;
+
+$font: #2f353f;
+$primary: rgb(76, 139, 247);
+$gray: rgba(0, 0, 0, 0.15);
+$white: #fff;
+
+button {
+  font-size: 1em;
+  font-weight: 600;
+
+  height: $height;
+  min-width: $height;
+  color: $font;
+  border-radius: 4px;
+  border: 1px solid $gray;
+  background-color: $white;
+  cursor: pointer;
+  padding: 0.6em;
+
+  &:hover {
+    color: $primary;
+    border-color: $primary;
+  }
+
+  &:active,
+  &.loading {
+    color: #fff;
+    background-color: $primary;
+  }
+
+  &.loading {
+    cursor: not-allowed;
+    padding: 0;
+    width: $height;
+  }
+}
+
+.loader {
+  width: 60%;
+  height: 60%;
+  border: 3px solid $white;
+  border-left-color: transparent;
+  border-bottom-color: transparent;
+  border-radius: 50%;
+  display: inline-block;
+  box-sizing: border-box;
+  animation: rotation 1s linear infinite;
+}
+@keyframes rotation {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
+</style>
